9fc80e0490e27c727e67f0571926f57b4ac2db0f
vishnudxb <vishnudxb@protonmail.com>
2023-09-15 22:01:30 -0400

    testing cluster sleep time

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ MASTER3_PORT=$3
 SLAVE1_PORT=$4
 SLAVE2_PORT=$5
 SLAVE3_PORT=$6
-SLEEP_DURATION=$7
+SLEEP_DURATION=${SLEEP_DURATION:-5} # Use 5 as the default if not provided
+
+# Ensure that SLEEP_DURATION is treated as an integer
+SLEEP_DURATION=$((SLEEP_DURATION))
 
 docker_run="docker run -d -p ${MASTER1_PORT}:6379 -p ${MASTER2_PORT}:6380 -p ${MASTER3_PORT}:6381 -p ${SLAVE1_PORT}:6382 -p ${SLAVE2_PORT}:6383 -p ${SLAVE3_PORT}:6384 --name redis-cluster vishnunair/docker-redis-cluster:latest"
 
@@ -17,8 +20,8 @@ sh -c "$docker_run"
 # Wait for Redis Cluster to be up and running
 echo "Waiting for Redis Cluster to start..."
 until docker exec redis-cluster redis-cli -h localhost -p 6379 cluster info; do
-    echo "Sleep duration of: ${SLEEP_DURATION}"
-    sleep "${SLEEP_DURATION}"
+    echo "Sleep duration of: $SLEEP_DURATION"
+    sleep "$SLEEP_DURATION"
 done
 
 # Perform additional health check here if needed
